82298bb18b099d7c53d075f54894663e437fdcfa
Gokul Sonawane <gsonawane@dynamichealthsys.com>
2018-03-09 17:14:40 +0530

    Test

---
+++ Algos/src/testone/teration/Channel.java
@@ -0,0 +1,25 @@
+package testone.teration;
+
+public class Channel {
+	
+	private double frequency;
+	private ChannelTypeEnum TYPE;
+	
+	public Channel(double frequency, ChannelTypeEnum tYPE) {
+		this.frequency = frequency;
+		this.TYPE = tYPE;
+	}
+	
+	public double getFrequency() {
+		return frequency;
+	}
+	public ChannelTypeEnum getTYPE() {
+		return TYPE;
+	}
+	
+	@Override
+	public String toString(){
+		return "Frequency="+this.frequency+", Type="+this.TYPE;
+	}
+
+}
---
+++ Algos/src/testone/teration/ChannelCollection.java
@@ -0,0 +1,11 @@
+package testone.teration;
+
+public interface ChannelCollection {
+	
+	public void addChannel(Channel c);
+	
+	public void removeChannel(Channel c);
+	
+	public ChannelIterator iterator(ChannelTypeEnum type);
+
+}
