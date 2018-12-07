cc0cdda4ccd43a3bbfd27e4906036c2645ad498c
Gokul <gokul@admin.com>
2018-12-07 17:21:10 +0530

    commit

---
+++ Algos/src/test/multithread/datashare/DataExchangeDemo.java
@@ -0,0 +1,21 @@
+package test.multithread.datashare;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+
+public class DataExchangeDemo {
+
+	public static void main(String[] args) {
+		BlockingQueue<DataInfo> message = new ArrayBlockingQueue<>(5);
+	   
+		DataRetrieving retrive = new DataRetrieving(message);
+		
+		DataDisplay  dsiplay = new DataDisplay(message);
+		
+		new Thread(retrive).start();
+		new Thread(dsiplay).start();
+		
+	}
+
+}
