26c798f1d55a450cf67ef4e58004943ac44765ff
Gokul <gokul@admin.com>
2018-12-12 17:15:13 +0530

    Hibernate commit

---
+++ HibernateTest/src/main/java/com/test/Vehicle.java
@@ -0,0 +1,44 @@
+package com.test;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="Vehicle")
+public class Vehicle {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column(name="VehicleName")
+	private String name;
+	
+	
+	
+	public Vehicle(String name) {
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
